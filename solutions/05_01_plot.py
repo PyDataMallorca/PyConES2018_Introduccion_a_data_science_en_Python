b0fa02101039b2782b4d5fad260df64ed83c5054
guillem <guillem.db@gmail.com>
2018-09-02 11:54:15 +0200

    Correct typos. Text cleanup

--- solutions/05_01_plot.py
+++ solutions/05_01_plot.py
@@ -1,13 +1,15 @@
 plt.figure(figsize=(16,7))
-plt.plot(x_total,y_total, color = 'black', label = '', linestyle="dashed", marker="")  # Dibujamos la evolución de f(x), frente a x
-
-plt.plot(set_ix, setosa, label = 'Setosa', linestyle="", marker="o", markersize=8, alpha=0.9, color="purple")
-plt.plot(versicolor_ix, versicolor, label = 'Versicolor', linestyle="", marker="o", markersize=8, alpha=0.9)
-plt.plot(virginica_ix, virginica, label = 'Virginica', linestyle="", marker="o", markersize=8, alpha=0.9, color="green")
-
-
+# Dibujamos la evolución de f(x), frente a x
+plt.plot(x_total,y_total, color = 'black', label = '', linestyle="dashed", marker="")  
+plt.plot(set_ix, setosa, label = 'Setosa', linestyle="", marker="o",
+         markersize=8, alpha=0.9, color="purple")
+plt.plot(versicolor_ix, versicolor, label = 'Versicolor', linestyle="",
+         marker="o", markersize=8, alpha=0.9)
+plt.plot(virginica_ix, virginica, label = 'Virginica', linestyle="",
+         marker="o", markersize=8, alpha=0.9, color="green")
+# Destacamos los valores por encima de 0.9 colocándoles un marcador circular azul
 plt.plot(x_total[y_total > 6.4], y_total[y_total > 6.4],
-         'bo', label = 'Long. sépalo > 0.9cm', markersize=3, color="yellow")  # Destacamos los valores por encima de 0.9 colocándoles un marcador circular azul
+         'bo', label = 'Long. sépalo > 0.9cm', markersize=3, color="yellow")  
 
 plt.axhspan(6.4, 8, alpha = 0.1)  # Colocamos una banda de color para los valores f(x) > 0.9
 plt.ylim(4,9)  # Limitamos el eje x
